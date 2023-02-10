39a8a670b30885b0469e422eac11bf5a4c290740
Aav <aav.verinhall@gmail.com>
2023-02-10 01:55:59 -0800

    Add post command

---
+++ cogs/utils.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from framework.bot import Bloo
+
+
+class Utility(commands.Cog):
+
+    def __init__(self, bot: Bloo):
+        self.bot = bot
+
+    @app_commands.command(name="post", description="Post a message to a channel")
+    @app_commands.default_permissions(manage_messages=True)
+    async def post(self, interaction: discord.Interaction, channel: discord.TextChannel, *, message: str):
+        await interaction.response.defer(ephemeral=True)
+        await channel.send(message)
+        await interaction.followup.send("Message sent!", ephemeral=True)
+
+
+async def setup(bot: Bloo):
+    await bot.add_cog(Utility(bot))
