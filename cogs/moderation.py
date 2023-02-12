cce47e8ec617ab95c31ae7726a34523686648441
Aav <aav.verinhall@gmail.com>
2023-02-11 20:39:59 -0800

    Fix mod cog.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,39 +12,33 @@ class Moderator(commands.Cog):
 
     @app_commands.command(name="ban", description="Ban a member by name, ID, or nation")
     @app_commands.default_permissions(ban_members=True)
-    async def ban(
-        self,
-        interaction: discord.Interaction,
-        member: discord.Member | str,
-        reason: str,
-        delete_message_days: int = 0,
+    async def ban_nation(
+            self,
+            interaction: discord.Interaction,
+            nation: str,
+            reason: str,
+            delete_message_days: int = 0,
     ):
         await interaction.response.defer(ephemeral=True)
-        if isinstance(member, str):
-            record = await self.bot.fetch(
-                "SELECT discord_id FROM nsv_table WHERE nation = $1",
-                member.lower().replace(" ", "_"),
+        record = await self.bot.fetch(
+            "SELECT discord_id FROM nsv_table WHERE nation = $1",
+            nation.lower().replace(" ", "_"),
+        )
+        if not nation:
+            await self.bot.execute(
+                "INSERT INTO nsv_ban_table (nation, reason, guild_id) VALUES ($1, $2, $3)",
+                nation.lower().replace(" ", "_"),
+                reason,
+                interaction.guild.id,
             )
-            if not member:
-                await self.bot.execute(
-                    "INSERT INTO nsv_ban_table (nation, reason, guild_id) VALUES ($1, $2, $3)",
-                    member.lower().replace(" ", "_"),
-                    reason,
-                    interaction.guild.id,
-                )
-                await interaction.followup.send(
-                    "Nation banned. On entry/verification, any user with that nation will be removed from the server.",
-                    ephemeral=True
-                )
-            await interaction.guild.ban(
-                record[0]["discord_id"], reason=reason, delete_message_days=delete_message_days
+            await interaction.followup.send(
+                "Nation banned. On entry/verification, any user with that nation will be removed from the server.",
+                ephemeral=True
             )
-            await interaction.followup.send("Banned!", ephemeral=True)
-        else:
-            await interaction.guild.ban(
-                member, reason=reason, delete_message_days=delete_message_days
-            )
-            await interaction.followup.send("Banned!", ephemeral=True)
+        await interaction.guild.ban(
+            record[0]["discord_id"], reason=reason, delete_message_days=delete_message_days
+        )
+        await interaction.followup.send("Banned!", ephemeral=True)
 
 
 async def setup(bot: Bloo):
