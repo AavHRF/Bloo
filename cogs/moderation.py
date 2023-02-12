4d9c2972bc54c69e5a83bb972a4ed4cc405acc9e
Aav <aav.verinhall@gmail.com>
2023-02-11 22:15:00 -0800

    Patch ban failure for ban_nation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -25,22 +25,22 @@ class Moderator(commands.Cog):
             "SELECT discord_id FROM nsv_table WHERE nation = $1",
             nation.lower().replace(" ", "_"),
         )
-        if not nation:
-            await self.bot.execute(
-                "INSERT INTO nsv_ban_table (nation, reason, guild_id) VALUES ($1, $2, $3)",
-                nation.lower().replace(" ", "_"),
-                reason,
-                interaction.guild.id,
-            )
-            await interaction.followup.send(
-                "Nation banned. On entry/verification, any user with that nation will be removed from the server.",
-                ephemeral=True,
-            )
-        await interaction.guild.ban(
-            record[0]["discord_id"],
-            reason=reason,
-            delete_message_days=delete_message_days,
+        await self.bot.execute(
+            "INSERT INTO nsv_ban_table (nation, reason, guild_id) VALUES ($1, $2, $3)",
+            nation.lower().replace(" ", "_"),
+            reason,
+            interaction.guild.id,
         )
+        await interaction.followup.send(
+            "Nation banned. On entry/verification, any user with that nation will be removed from the server.",
+            ephemeral=True,
+        )
+        if record:
+            await interaction.guild.ban(
+                record[0]["discord_id"],
+                reason=reason,
+                delete_message_days=delete_message_days,
+            )
         await interaction.followup.send("Banned!", ephemeral=True)
 
 
