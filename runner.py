07236a6ff5c97affb37178e78599d4da6fbb917b
Aav <aav.verinhall@gmail.com>
2023-03-10 21:11:12 -0800

    Add log-to-file with a rotating log handler

--- runner.py
+++ runner.py
@@ -1,5 +1,15 @@
 from framework.bot import Bloo
+import logging
+import logging.handlers
+
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
 
 if __name__ == "__main__":
     bot = Bloo()
-    bot.run()
+    bot.run(log_handler=handler, log_level=logging.DEBUG)
