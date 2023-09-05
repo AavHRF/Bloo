d724a8c6d8b60360254593abc8c46aef456b1068
Aav <aav.verinhall@gmail.com>
2023-09-05 13:29:22 -0700

    Change length checking, strip % symbol

--- cogs/watchlist.py
+++ cogs/watchlist.py
@@ -134,7 +134,7 @@ class SearchBox(discord.ui.Modal, title="Search"):
                 if count == 1:
                     embed = watchlist_embed(record[0])
                     await interaction.followup.send(
-                        f"Your search for `{query}` returned the following result:",
+                        f"Your search for `{query.strip('%')}` returned the following result:",
                         embed=embed
                     )
                 else:
@@ -253,7 +253,7 @@ class PaginateWL(discord.ui.View):
         emoji="⬅️",
     )
     async def previous_page(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.current_page != 0:
+        if self.current_page > 0:
             self.current_page = len(self.watchlistitems) - 1
         else:
             self.current_page = 0
@@ -268,7 +268,7 @@ class PaginateWL(discord.ui.View):
         emoji="➡️",
     )
     async def next_page(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.current_page != len(self.watchlistitems) - 1:
+        if self.current_page <= len(self.watchlistitems) - 1:
             self.current_page += 1
         else:
             self.current_page = 0
